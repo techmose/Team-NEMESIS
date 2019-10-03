39b2e8f752fe0cb1a6d1969b63d6ea0f091093de
Elechi Golden <36916047+techmose@users.noreply.github.com>
2019-10-03 22:11:43 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -26,7 +26,7 @@ $model->store_tweets();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>Dashboard</title>
+  <title>1Dashboard</title>
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
